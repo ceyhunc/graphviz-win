45777e4b7416e51610c96e772c22f95fe261864b
ceyhunc <ceyhunc@gmail.com>
2013-02-28 07:02:01 +0200

    simpledot'u ekledim

---
+++ denemeler/simpledot/simpledot.cpp
@@ -0,0 +1,28 @@
+// simpledot.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "config.h"
+#include "gvc.h"
+
+static GVC_t *Gvc;
+static graph_t * G;
+
+char *Info[]={
+	"simpledot", "1.0", "2013"
+};
+
+double _HUGE = 1e16;
+
+int _tmain(int argc, char* argv[])
+{
+	Gvc = gvNEWcontext(Info, gvUsername());
+	gvParseArgs(Gvc, argc, argv);
+	G=next_input_graph();
+	gvLayoutJobs(Gvc, G);  /* take layout engine from command line */
+	gvRenderJobs(Gvc, G);
+	gvFreeContext(Gvc);
+
+	return 0;
+}
+
